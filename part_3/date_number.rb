66bbfad043d6eabffc2e5c31b925268629f57a43
Natalya <nikifor1114@mail.ru>
2022-05-20 18:11:22 +0700

    Fix high_year method

--- part_3/date_number.rb
+++ part_3/date_number.rb
@@ -4,7 +4,7 @@ puts 'Введите число.месяц.год через точку'
 day, month, year = gets.chomp.gsub(/\s+/, '').split('.').map(&:to_i)
 
 def high_year?(year)
-  (year % 4).zero? && (year % 100).zero? && (year % 400).zero?
+  (year % 4).zero? && (!(year % 100).zero? || ((year % 100).zero? && (year % 400).zero?))
 end
 
 def data_valid?(day, month, year)
@@ -17,18 +17,18 @@ end
 days_in_february = ->(year) { high_year?(year) ? 29 : 28 }
 
 DAYS_IN_MONTH = {
-  1  => { label: :January,   day: 31 },
-  2  => { label: :February,  day: days_in_february.call(year) },
-  3  => { label: :March,     day: 31 },
-  4  => { label: :April,     day: 30 },
-  5  => { label: :May,       day: 31 },
-  6  => { label: :June,      day: 30 },
-  7  => { label: :July,      day: 31 },
-  8  => { label: :August,    day: 31 },
-  9  => { label: :September, day: 30 },
-  10 => { label: :October,   day: 31 },
-  11 => { label: :November,  day: 30 },
-  12 => { label: :December,  day: 31 }
+  1  => { label: :january,   day: 31 },
+  2  => { label: :february,  day: days_in_february.call(year) },
+  3  => { label: :march,     day: 31 },
+  4  => { label: :april,     day: 30 },
+  5  => { label: :may,       day: 31 },
+  6  => { label: :june,      day: 30 },
+  7  => { label: :july,      day: 31 },
+  8  => { label: :august,    day: 31 },
+  9  => { label: :september, day: 30 },
+  10 => { label: :october,   day: 31 },
+  11 => { label: :november,  day: 30 },
+  12 => { label: :december,  day: 31 }
 }.freeze
 
 until data_valid?(day, month, year) do
